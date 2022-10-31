82ab1d908781e2be4cdfeced38ff89a603ff3d8f
Carina George <carinageorge@Devins-MacBook-Pro-2.local>
2022-10-31 12:25:02 -0400

    Adding comments to Organizers.ts

--- src/Organizers/Organizers.ts
+++ src/Organizers/Organizers.ts
@@ -65,51 +65,157 @@ class Organizers {
     };
 
     
-
+    /**
+     * Show a list organizer associated with the current JSON Web Token.
+     * 
+     * @see [Organizers List - BingeWave](https://developers.bingewave.com/docs/organizers#list)
+     * 
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static getOrganizers(query? : object | null, options? : object | null) {
         return Requests.get(this.routeListOrganizers.route, query);
     }
 
+    /**
+     * Creates an organizer account.
+     * 
+     * @see [Organizers Create - BingeWave](https://developers.bingewave.com/docs/organizers#create)
+     * 
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static createOrganizer(data : object, query? : object | null, options? : object | null) {
         return Requests.post(this.routeCreateOrganizer.route, data, query, options);
     }
 
+    /**
+     * Update the information to an organizer account.
+     * 
+     * @see [Organizers Update Organizer - BingeWave](https://developers.bingewave.com/docs/organizers#update)
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static updateOrganizer(organizer_id: string, data : object, query? : object | null, options? : object | null) {
         let route = this.routeUpdateOrganizer.route.replaceAll('{id}', organizer_id);
         return Requests.put(route, data, query, options);
     }
 
+    /**
+     * View the information pertaining to a single organizer account.
+     * 
+     * @see [Organizers View Organizer - BingeWave](https://developers.bingewave.com/docs/organizers#view)
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static viewOrganizer(organizer_id: string, query? : object | null, options? : object | null) {
         let route = this.routeViewOrganizer.route.replaceAll('{id}', organizer_id);
         return Requests.get(route, query, options);
     }
 
+    /**
+     * Deletes organizer account.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static deleteOrganizer(organizer_id: string, data : object, query? : object | null, options? : object | null) {
         let route = this.routeDeleterOrganizer.route.replaceAll('{id}', organizer_id);
         return Requests.delete(route, data, query, options);
     }
 
+    /**
+     * Set a user's role for how much access they will have on the platform.
+     * 
+     * @see [Organizers Set Role - BingeWave](https://developers.bingewave.com/docs/organizersmanage#setuser)
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static setUserToRole(organizer_id: string, data : object, query? : object | null, options? : object | null) {
         let route = this.routeSetUserToRoleWithOganizer.route.replaceAll('{id}', organizer_id);
         return Requests.post(route, data, query, options);
     }
 
+    /**
+     * Removes an account from a role for the current organizer account.
+     * 
+     * @see [Organizers Remove Account From Role - BingeWave](https://developers.bingewave.com/docs/organizersmanage#removeuser)
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static removeUserFromRole(organizer_id: string, data : object, query? : object | null, options? : object | null) {
         let route = this.routeRemoveUserFromRoleWithOganizer.route.replaceAll('{id}', organizer_id);
         return Requests.post(route, data, query, options);
     }
 
+    /**
+     * Creates subscription for organizer account.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static createSubscription(organizer_id: string, data : object, query? : object | null, options? : object | null) {
         let route = this.routeCreateSubscription.route.replaceAll('{id}', organizer_id);
         return Requests.post(route, data, query, options);
     }
 
+    /**
+     * Retrieves subscription information from organizer account.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static getSubscriptions(organizer_id: string, query? : object | null, options? : object | null) {
         let route = this.routeGetSubscriptions.route.replaceAll('{id}', organizer_id);
 
         return Requests.get(route, query, options);
     }
 
+    /**
+     * View the information pertaining to subscription.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param subscription_id The id of subscription.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static viewSubscription(organizer_id: string, subscription_id: string,query? : object | null, options? : object | null) {
         let route = this.routeSingleSubscription.route.replaceAll('{id}', organizer_id);
 
@@ -118,12 +224,31 @@ class Organizers {
         return Requests.get(route, query, options);
     }
 
+    /**
+     * Retrieves the information pertaining to current subscription.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static getCurrentSubscription(organizer_id: string, query? : object | null, options? : object | null) {
         let route = this.routeCurrentSubscription.route.replaceAll('{id}', organizer_id);
 
         return Requests.get(route, query, options);
     }
 
+    /**
+     * Cancles subscription.
+     * 
+     * @param organizer_id The id of the organizer account.
+     * @param data Data that will be passed in the body of the request.
+     * @param query Data that will be passed in the query string as a parameter.
+     * @param options Further options that can be used to modify the request.
+     * 
+     * @returns Returns a promise from Axios.
+     */
     public static cancelSubscription(organizer_id: string, data? : object | null, query? : object | null, options? : object | null) {
         let route = this.routeCancelSubscription.route.replaceAll('{id}', organizer_id);
 
